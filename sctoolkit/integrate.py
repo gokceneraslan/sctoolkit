1ea157bc4547de445cf85fbd578f5050076d3b32
Gokcen Eraslan <geraslan@broadinstitute.org>
2020-10-24 23:18:24 -0400

    Add more integration bits

--- sctoolkit/integrate.py
+++ sctoolkit/integrate.py
@@ -1,16 +1,24 @@
 import scanpy as sc
+import numpy as np
 
 from .disvae.disvae.singlecell import fit_single_cell
 
 def integrate(adata, categorical_vars, experiment_name='tcbetavae',beta=2., count_layer='counts', n_hvg=5000, cuda=False, latent_dim=64, epochs=150, output_activation='softplus'):
 
-    adata.X = adata.layers[count_layer].copy()
-    sc.pp.highly_variable_genes(adata, n_top_genes=n_hvg, subset=True, flavor='seurat_v3')
-    sc.pp.normalize_total(adata, target_sum=10000)
-    sc.pp.log1p(adata)
+    ad = adata.copy()
+    ad.X = ad.layers[count_layer].copy()
+    sc.pp.highly_variable_genes(ad, n_top_genes=n_hvg, subset=True, flavor='seurat_v3')
+    sc.pp.normalize_total(ad, target_sum=10000)
+    sc.pp.log1p(ad)
 
-    ad, model, trainer, train_loader = fit_single_cell(adata.copy(), experiment_name, btcvae_B=beta, cuda=cuda,
+    ad, model, trainer, train_loader = fit_single_cell(ad.copy(), experiment_name, btcvae_B=beta, cuda=cuda,
                                                        categorical_vars=categorical_vars,
                                                        latent_dim=latent_dim, epochs=epochs, output_activation=output_activation)
 
-    return ad, model
+    assert np.all(adata.obs_names == ad.obs_names)
+
+    adata.obsm['X_vae_samples'] = ad.obsm['X_vae_samples']
+    adata.obsm['X_vae_mean'] = ad.obsm['X_vae_mean']
+    adata.obsm['X_vae_var'] = ad.obsm['X_vae_var']
+
+    return adata, model
